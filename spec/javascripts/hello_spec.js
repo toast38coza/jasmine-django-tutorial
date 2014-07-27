af5efa701eff211d4f40f47cfaf9f59a2fc4655b
Christo Crampton <info@38.co.za>
2014-07-27 14:33:01 +0200

    AJAX mocking examples

--- spec/javascripts/hello_spec.js
+++ spec/javascripts/hello_spec.js
@@ -1,6 +1,45 @@
 describe("Test hello.js", function() {
+
+  beforeEach(function() {
+      jasmine.Ajax.install();
+  });
+
+  afterEach(function() {
+      jasmine.Ajax.uninstall();
+  });
+
+
   it("return 'hello'", function () {
   	var result = Hello.world();
   	expect(result).toBe("Hello");
+  });
+
+  it ("Logs text from the service to the console", function () {
+  	
+  	var consoleSpy = spyOn(console, "log");
+
+  	jasmine.Ajax.stubRequest('/some/url').andReturn({
+        "status": 200, 
+        "contentType": 'text/plain',
+        "responseText": 'Hello from the world'
+    });
+
+  	var result = Hello.callWorld();
+  	expect(consoleSpy).toHaveBeenCalledWith("Hello from the world");
+  	expect(consoleSpy).toHaveBeenCalledWith("complete");
+  });
+
+  it ("Prints error if the service fails", function () {
+  	
+  	var consoleSpy = spyOn(console, "log");
+
+  	jasmine.Ajax.stubRequest('/some/url').andReturn({
+        "status": 500, 
+        "contentType": 'text/plain'
+    });
+
+  	var result = Hello.callWorld();
+  	expect(consoleSpy).toHaveBeenCalledWith("error");
+  	expect(consoleSpy).toHaveBeenCalledWith("complete");
   });
 });
--- static/js/hello.js
+++ static/js/hello.js
@@ -1,5 +1,18 @@
 var Hello = {
 	world : function () {
 		return "Hello";
+	},
+
+	callWorld : function () {
+		var xhr = $.ajax('/some/url')
+			.done(function(responseText) { 
+				console.log( responseText ); 
+			})
+  			.fail(function() { 
+  				console.log( "error" ); 
+  			})
+  			.always(function() { 
+  				console.log( "complete" ); 
+  			});
 	}
 };
